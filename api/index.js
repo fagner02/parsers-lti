59829955e5245f9a5f0ee50e30bf63ccc2681c55
fagner ferreira <fagnerrc12@gmail.com>
2024-05-18 18:38:24 -0300

    update

--- api/index.js
+++ api/index.js
@@ -1,29 +1,60 @@
-// Require Provider
-import { Provider as lti } from "ltijs";
+const lti = require("ltijs").Provider;
 
-// Setup provider
 lti.setup(
-  process.env.client, // Key used to sign cookies and tokens
+  process.env.client,
   {
-    // Database configuration
     url: process.env.connection,
   },
   {
-    // Options
-    appRoute: "/",
-    loginRoute: "/login", // Optionally, specify some of the reserved routes
     cookies: {
-      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: "", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      secure: false,
+      sameSite: "",
     },
-    devMode: true, // Set DevMode to false if running in a production environment with https
+    devMode: true,
+    dynReg: {
+      url: process.env.url,
+      name: "Tool Provider",
+      logo: "https://parsers-svelte.netlify.app/favicon.png",
+      description: "Tool Description",
+      redirectUris: [process.env.url],
+      autoActivate: true,
+    },
   }
 );
+
+lti.onDynamicRegistration(async (req, res, next) => {
+  try {
+    if (!req.query.openid_configuration)
+      return res.status(400).send({
+        status: 400,
+        error: "Bad Request",
+        details: { message: 'Missing parameter: "openid_configuration".' },
+      });
+    const message = await lti.DynamicRegistration.register(
+      req.query.openid_configuration,
+      req.query.registration_token
+    );
+    res.setHeader("Content-type", "text/html");
+    res.send(message);
+  } catch (err) {
+    if (err.message === "PLATFORM_ALREADY_REGISTERED")
+      return res.status(403).send({
+        status: 403,
+        error: "Forbidden",
+        details: { message: "Platform already registered." },
+      });
+    return res.status(500).send({
+      status: 500,
+      error: "Internal Server Error",
+      details: { message: err.message },
+    });
+  }
+});
 lti.app.post("/grade", async (req, res) => {
   try {
-    const idtoken = res.locals.token; // IdToken
-    const score = req.body.grade; // User numeric score sent in the body
-    // Creating Grade object
+    const idtoken = res.locals.token;
+    const score = req.body.grade;
+
     const gradeObj = {
       userId: idtoken.user,
       scoreGiven: score,
@@ -32,15 +63,13 @@ lti.app.post("/grade", async (req, res) => {
       gradingProgress: "FullyGraded",
     };
 
-    // Selecting linetItem ID
-    let lineItemId = idtoken.platformContext.endpoint.lineitem; // Attempting to retrieve it from idtoken
+    let lineItemId = idtoken.platformContext.endpoint.lineitem;
     if (!lineItemId) {
       const response = await lti.Grade.getLineItems(idtoken, {
         resourceLinkId: true,
       });
       const lineItems = response.lineItems;
       if (lineItems.length === 0) {
-        // Creating line item if there is none
         console.log("Creating new line item");
         const newLineItem = {
           scoreMaximum: 100,
@@ -53,7 +82,6 @@ lti.app.post("/grade", async (req, res) => {
       } else lineItemId = lineItems[0].id;
     }
 
-    // Sending Grade
     const responseGrade = await lti.Grade.submitScore(
       idtoken,
       lineItemId,
@@ -65,72 +93,13 @@ lti.app.post("/grade", async (req, res) => {
     return res.status(500).send({ err: err.message });
   }
 });
-// Set lti launch callback
+
 lti.onConnect(async (token, req, res) => {
-  try {
-    const idtoken = res.locals.token; // IdToken
-    const score = req.body.grade; // User numeric score sent in the body
-    // Creating Grade object
-    req.token;
-    const gradeObj = {
-      userId: idtoken.user,
-      scoreGiven: 50,
-      scoreMaximum: 100,
-      activityProgress: "Completed",
-      gradingProgress: "FullyGraded",
-    };
-
-    // Selecting linetItem ID
-    let lineItemId = idtoken.platformContext.endpoint.lineite; // Attempting to retrieve it from idtoken
-    if (!lineItemId) {
-      const response = await lti.Grade.getLineItems(idtoken, {
-        resourceLinkId: true,
-      });
-      const lineItems = response.lineItems;
-      if (lineItems.length === 0) {
-        // Creating line item if there is none
-        console.log("Creating new line item");
-        const newLineItem = {
-          scoreMaximum: 100,
-          label: "Grade",
-          tag: "grade",
-          resourceLinkId: idtoken.platformContext.resource.id,
-        };
-        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem);
-        lineItemId = lineItem.id;
-      } else lineItemId = lineItems[0].id;
-    }
-
-    // Sending Grade
-    const responseGrade = await lti.Grade.submitScore(
-      idtoken,
-      lineItemId,
-      gradeObj
-    );
-    return res.send(responseGrade);
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).send({ err: err.message });
-  }
   return res.send("It's alive!");
 });
 
 const setup = async () => {
-  // Deploy server and open connection to the database
-  await lti.deploy({ port: 3000, serverless: true }); // Specifying port. Defaults to 3000
-
-  // Register platform
-  // await lti.registerPlatform({
-  //   url: process.env.url,
-  //   name: process.env.name,
-  //   clientId: process.env.client,
-  //   authenticationEndpoint: process.env.authenticationEndpoint,
-  //   accesstokenEndpoint: process.env.accesstokenEndpoint,
-  //   authConfig: {
-  //     method: "JWK_SET",
-  //     key: process.env.key,
-  //   },
-  // });
+  await lti.deploy({ port: 3000 });
 };
 
 setup();
