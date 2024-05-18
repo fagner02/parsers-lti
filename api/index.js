7877d25ba79769a8e8e2dbecd9f15c5d98e7a390
fagner ferreira <fagnerrc12@gmail.com>
2024-05-18 20:41:24 -0300

    update

--- api/index.js
+++ api/index.js
@@ -21,34 +21,35 @@ lti.setup(
   }
 );
 
-// lti.onDynamicRegistration(async (req, res, next) => {
-//   try {
-//     if (!req.query.openid_configuration)
-//       return res.status(400).send({
-//         status: 400,
-//         error: "Bad Request",
-//         details: { message: 'Missing parameter: "openid_configuration".' },
-//       });
-//     const message = await lti.DynamicRegistration.register(
-//       req.query.openid_configuration,
-//       req.query.registration_token
-//     );
-//     res.setHeader("Content-type", "text/html");
-//     res.send(message);
-//   } catch (err) {
-//     if (err.message === "PLATFORM_ALREADY_REGISTERED")
-//       return res.status(403).send({
-//         status: 403,
-//         error: "Forbidden",
-//         details: { message: "Platform already registered." },
-//       });
-//     return res.status(500).send({
-//       status: 500,
-//       error: "Internal Server Error",
-//       details: { message: err },
-//     });
-//   }
-// });
+lti.onDynamicRegistration(async (req, res, next) => {
+  res.send(process.env);
+  // try {
+  //   if (!req.query.openid_configuration)
+  //     return res.status(400).send({
+  //       status: 400,
+  //       error: "Bad Request",
+  //       details: { message: 'Missing parameter: "openid_configuration".' },
+  //     });
+  //   const message = await lti.DynamicRegistration.register(
+  //     req.query.openid_configuration,
+  //     req.query.registration_token
+  //   );
+  //   res.setHeader("Content-type", "text/html");
+  //   res.send(message);
+  // } catch (err) {
+  //   if (err.message === "PLATFORM_ALREADY_REGISTERED")
+  //     return res.status(403).send({
+  //       status: 403,
+  //       error: "Forbidden",
+  //       details: { message: "Platform already registered." },
+  //     });
+  //   return res.status(500).send({
+  //     status: 500,
+  //     error: "Internal Server Error",
+  //     details: { message: err },
+  //   });
+  // }
+});
 lti.app.post("/grade", async (req, res) => {
   try {
     const idtoken = res.locals.token;
