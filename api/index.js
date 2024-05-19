b49166d252aed7299b19bce2d2465d823cc57867
fagner ferreira <fagnerrc12@gmail.com>
2024-05-18 22:33:01 -0300

    update

--- api/index.js
+++ api/index.js
@@ -12,7 +12,6 @@ lti.setup(
       secure: false,
       sameSite: "",
     },
-    cors: true,
     devMode: false,
     dynReg: {
       url: process.env.url,
@@ -23,12 +22,6 @@ lti.setup(
       autoActivate: true,
     },
   }
-);
-
-lti.app.use(
-  cors({
-    origin: "*",
-  })
 );
 
 lti.onDynamicRegistration(async (req, res, next) => {
@@ -104,7 +97,48 @@ lti.app.post("/grade", async (req, res) => {
 });
 
 lti.onConnect(async (token, req, res) => {
-  return res.send("It's alive!");
+  try {
+    const idtoken = res.locals.token;
+    const score = req.body.grade;
+
+    const gradeObj = {
+      userId: idtoken.user,
+      scoreGiven: score,
+      scoreMaximum: 100,
+      activityProgress: "Completed",
+      gradingProgress: "FullyGraded",
+    };
+
+    let lineItemId = idtoken.platformContext.endpoint.lineitem;
+    if (!lineItemId) {
+      const response = await lti.Grade.getLineItems(idtoken, {
+        resourceLinkId: true,
+      });
+      const lineItems = response.lineItems;
+      if (lineItems.length === 0) {
+        console.log("Creating new line item");
+        const newLineItem = {
+          scoreMaximum: 100,
+          label: "Grade",
+          tag: "grade",
+          resourceLinkId: idtoken.platformContext.resource.id,
+        };
+        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem);
+        lineItemId = lineItem.id;
+      } else lineItemId = lineItems[0].id;
+    }
+
+    const responseGrade = await lti.Grade.submitScore(
+      idtoken,
+      lineItemId,
+      gradeObj
+    );
+    return res.send(responseGrade);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ err: err.message });
+  }
+  return res.redirect("https://parsers-svelte.netlify.app/");
 });
 
 const setup = async () => {
