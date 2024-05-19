57fc0aec1123d6fca931e076dcb7f419a957cf2a
fagner ferreira <fagnerrc12@gmail.com>
2024-05-18 23:02:15 -0300

    update

--- api/index.js
+++ api/index.js
@@ -9,8 +9,8 @@ lti.setup(
   },
   {
     cookies: {
-      secure: true,
-      sameSite: "None",
+      secure: false,
+      sameSite: "",
     },
     devMode: false,
     dynReg: {
@@ -96,51 +96,54 @@ lti.app.post("/grade", async (req, res) => {
   }
 });
 
-lti.onConnect(async (token, req, res) => {
-  try {
-    const idtoken = res.locals.token;
-    const score = req.body.grade;
+lti.onConnect(
+  async (token, req, res) => {
+    try {
+      const idtoken = res.locals.token;
+      const score = req.body.grade;
 
-    const gradeObj = {
-      userId: idtoken.user,
-      scoreGiven: 50,
-      scoreMaximum: 100,
-      activityProgress: "Completed",
-      gradingProgress: "FullyGraded",
-    };
+      const gradeObj = {
+        userId: idtoken.user,
+        scoreGiven: 50,
+        scoreMaximum: 100,
+        activityProgress: "Completed",
+        gradingProgress: "FullyGraded",
+      };
 
-    const result = await lti.Grade.result(idtoken);
-    // let lineItemId = idtoken.platformContext.endpoint.lineitem;
-    // if (!lineItemId) {
-    //   const response = await lti.Grade.getLineItems(idtoken, {
-    //     resourceLinkId: true,
-    //   });
-    //   const lineItems = response.lineItems;
-    //   if (lineItems.length === 0) {
-    //     console.log("Creating new line item");
-    //     const newLineItem = {
-    //       scoreMaximum: 100,
-    //       label: "Grade",
-    //       tag: "grade",
-    //       resourceLinkId: idtoken.platformContext.resource.id,
-    //     };
-    //     const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem);
-    //     lineItemId = lineItem.id;
-    //   } else lineItemId = lineItems[0].id;
-    // }
+      const result = await lti.Grade.result(idtoken);
+      // let lineItemId = idtoken.platformContext.endpoint.lineitem;
+      // if (!lineItemId) {
+      //   const response = await lti.Grade.getLineItems(idtoken, {
+      //     resourceLinkId: true,
+      //   });
+      //   const lineItems = response.lineItems;
+      //   if (lineItems.length === 0) {
+      //     console.log("Creating new line item");
+      //     const newLineItem = {
+      //       scoreMaximum: 100,
+      //       label: "Grade",
+      //       tag: "grade",
+      //       resourceLinkId: idtoken.platformContext.resource.id,
+      //     };
+      //     const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem);
+      //     lineItemId = lineItem.id;
+      //   } else lineItemId = lineItems[0].id;
+      // }
 
-    // const responseGrade = await lti.Grade.submitScore(
-    //   idtoken,
-    //   lineItemId,
-    //   gradeObj
-    // );
-    return res.send(result);
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).send({ err: err.message });
-  }
-  return res.redirect("https://parsers-svelte.netlify.app/");
-});
+      // const responseGrade = await lti.Grade.submitScore(
+      //   idtoken,
+      //   lineItemId,
+      //   gradeObj
+      // );
+      return res.send(result);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send({ err: err.message });
+    }
+    return res.redirect("https://parsers-svelte.netlify.app/");
+  },
+  { sameSite: "None" }
+);
 
 const setup = async () => {
   await lti.deploy({ port: 3000 });
